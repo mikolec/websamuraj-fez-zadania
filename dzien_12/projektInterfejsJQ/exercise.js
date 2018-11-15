aee36d2a5e9432a803ce32e645ea716ef41d1464
Mikołaj Lechtański <mikolec@gmail.com>
2018-11-15 19:23:52 +0100

    Dzień 12: Projekt Interfejs - JQ - opcje poprawkowe po obejrzeniu rozwiązania

--- dzien_12/projektInterfejsJQ/exercise.js
+++ dzien_12/projektInterfejsJQ/exercise.js
@@ -1,3 +1,4 @@
+/*
 $('.interface.orange').on('click', function () {
     $('body').css("background-color", "orange");
 });
@@ -15,4 +16,35 @@ $('.interface.move').on('click', function () {
         "letter-spacing": "+=1",
         left: "+=10"
     }, 500);
+});
+*/
+
+$('.interface').on('click', function () {
+    if ($(this).hasClass('orange')) {
+        // $('body').css("background-color", "orange");
+        // $('body').removeClass();
+        // $('body').addClass('orange');
+        // $('body').css({
+        //     "background-color": "orange"
+        // });
+        $('body').attr('class', 'orange');
+    }
+    if ($(this).hasClass('green')) {
+        // $('body').css({
+        //     "background-color": "green"
+        // });
+        $('body').attr('class', 'green');
+    }
+    if ($(this).hasClass('increase')) {
+        $('.text').animate({
+            "font-size": "+=10"
+        }, 500);
+    }
+    if ($(this).hasClass('move')) {
+        $('.text').animate({
+            "letter-spacing": "+=2",
+            left: "+=10"
+        }, 500);
+    }
+
 });
