ffc6f9736d1d228578e31c20ab6d917a08a88af7
Mikołaj Lechtański <mikolec@gmail.com>
2018-11-16 12:37:46 +0100

    Dzień 13: projekt 1d: menu hamburger w JQ - skrócony zapis

--- dzien_13/hamburger4/Proj4-MenuBoczneJQ/script.js
+++ dzien_13/hamburger4/Proj4-MenuBoczneJQ/script.js
@@ -1,17 +1,3 @@
-// const burger = document.querySelector('.burger'),
-//     burgerIcon = burger.querySelector('.fa-bars'),
-//     closeIcon = burger.querySelector('.fa-times'),
-//     menu = document.querySelector('aside');
-
-// burger.addEventListener('click', function () {
-//     burgerIcon.classList.toggle('show');
-//     closeIcon.classList.toggle('show');
-//     menu.classList.toggle('show');
-// });
-
 $('.burger').on('click', function () {
-    $('.fa-bars').toggleClass('show');
-    $('.fa-times').toggleClass('show');
-    $('aside').toggleClass('show');
-
-})
+    $('.fas, aside').toggleClass('show'); //JQ wykona pętle dla każdego pobranego elementu
+});
